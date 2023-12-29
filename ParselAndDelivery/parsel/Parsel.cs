a5ba6e0e60e2d9b41dbc15877f7a3b9d4ba9caf1
Tsuniti <Tsuniti00@gmail.com>
2023-12-29 12:50:37 +0200

    Parsel.cs update

--- ParselAndDelivery/parsel/Parsel.cs
+++ ParselAndDelivery/parsel/Parsel.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ParselAndDelivery.parsel
+namespace ParselAndDelivery.parsel;
+
+public class Parsel
 {
-    internal class Parsel
+    public List <Product> Products = new List<Product>();
+    public string Customer;
+
+
+
+    public void AddProduct(string name, int price, double weight)
     {
+        Products.Add(new Product { Name = name, Price = price, Weight = weight });
+    }
+    public int GetFullPrice()
+    {
+        int result = 0;
+        foreach (var item in Products)
+        {
+            result += item.Price;
+        }
+        return result;
+    }
+    public double GetWeight() 
+    {
+        double result = 0;
+        foreach (var item in Products)
+        {
+            result += item.Weight;
+        }
+        return result;
     }
 }
